f39144835f04d21058cde5191c47b465a64c185f
seekCat969 <zhangmingzhuo969@gmail.com>
2025-04-10 18:54:39 +0800

    修改了一些配置

---
+++ src/main/java/com/seekcat/tlias/TliasApplication.java
@@ -0,0 +1,13 @@
+package com.seekcat.tlias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TliasApplication.class, args);
+	}
+
+}
