1a45d406fc5a50735a1e139bdc68689501229322
seekCat969 <zhangmingzhuo969@gmail.com>
2025-04-15 22:27:33 +0800

    一些Springboot项目的框架文件

--- src/main/java/com/seekcat/tlias/controller/EmpController.java
+++ src/main/java/com/seekcat/tlias/controller/EmpController.java
@@ -9,13 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.UUID;
-import java.util.function.Consumer;
 
 @Slf4j
 @RestController
@@ -48,6 +46,8 @@ public class EmpController {
         return Result.success();
     }
 
+    /**
+     *插入员工*/
     @PostMapping()
     public Result InsertEmp(@RequestBody Emp emp) {
         log.info("debug start");
@@ -56,16 +56,4 @@ public class EmpController {
         return Result.success();
     }
 
-    @PostMapping("/upload")
-    public Result uploadImage(MultipartFile image) throws IOException {
-        String[] str = image.getOriginalFilename().split("\\.");
-        StringBuilder sb = new StringBuilder();
-
-        Arrays.stream(UUID.randomUUID().toString().split("-")).forEach(sb::append);
-
-        String fileName = sb.toString() + '.' + str[str.length - 1];
-        image.transferTo(new File("D:\\code\\MD文档", fileName));
-
-        return Result.success();
-    }
 }
